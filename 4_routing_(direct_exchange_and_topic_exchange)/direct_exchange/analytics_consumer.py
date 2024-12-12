70d13ea558bd6c230d8461959afff7f9ee9dcd10
Amirhossein Adibinia <amir.adibinia@gmail.com>
2024-12-12 20:34:06 +0330

    feat: add routing pattern using direct exchange

---
+++ 4_routing_(direct_exchange_and_topic_exchange)/direct_exchange/analytics_consumer.py
@@ -0,0 +1,25 @@
+import pika
+from pika.adapters.blocking_connection import BlockingChannel
+from pika.exchange_type import ExchangeType
+from pika.spec import Basic, BasicProperties
+from pika.frame import Method
+
+
+def on_message_received(ch: BlockingChannel, method: Basic.Deliver, properties: BasicProperties, body: bytes):
+    print(f"Analytics Service - received new message: {body}")
+
+connection_parameters = pika.ConnectionParameters(host='localhost', port=5672)
+connection = pika.BlockingConnection(connection_parameters)
+
+channel = connection.channel()
+
+channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.direct)
+
+queue: Method = channel.queue_declare(queue='', exclusive=True)
+
+channel.queue_bind(exchange='routing', queue=queue.method.queue, routing_key='analytics_only')
+
+channel.basic_consume(queue=queue.method.queue, auto_ack=True, on_message_callback=on_message_received)
+
+print('Starting Consuming')
+channel.start_consuming()
