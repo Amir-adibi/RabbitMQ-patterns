b9f933cac2280f360d1eeb472022283f2cd569b1
Amirhossein Adibinia <amir.adibinia@gmail.com>
2024-12-12 20:36:01 +0330

    feat: change routing pattern

--- 4_routing_(direct_exchange_and_topic_exchange)/direct_exchange/analytics_consumer.py
+++ 4_routing_(direct_exchange_and_topic_exchange)/direct_exchange/analytics_consumer.py
@@ -18,6 +18,7 @@ channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.direct)
 queue: Method = channel.queue_declare(queue='', exclusive=True)
 
 channel.queue_bind(exchange='routing', queue=queue.method.queue, routing_key='analytics_only')
+channel.queue_bind(exchange='routing', queue=queue.method.queue, routing_key='both')
 
 channel.basic_consume(queue=queue.method.queue, auto_ack=True, on_message_callback=on_message_received)
 
--- 4_routing_(direct_exchange_and_topic_exchange)/direct_exchange/producer.py
+++ 4_routing_(direct_exchange_and_topic_exchange)/direct_exchange/producer.py
@@ -11,6 +11,9 @@ channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.direct)
 message = 'This message needs to be routed'
 channel.basic_publish(exchange='routing', routing_key='analytics_only', body=message)
 
+message = 'Broadcast message'
+channel.basic_publish(exchange='routing', routing_key='both', body=message)
+
 print(f"sent message {message}")
 
 connection.close()
